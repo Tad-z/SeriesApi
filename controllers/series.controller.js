e25695aba658034db2811344a4dcbe5e21453353
Akintade David <david.akintade1000@gmail.com>
2022-09-28 10:42:35 +0100

    changes

--- controllers/series.controller.js
+++ controllers/series.controller.js
@@ -1,82 +1,82 @@
 const Series = require("../models/series.js");
-const paginatedResults = require("./paginatedResults.js")
 
-exports.postSeries = async (req,res) => {
-    try {
-        const series = new Series({
-            image: req.file.path,
-            name: req.body.name,
-            genre: req.body.genre,
-            FavCast: req.body.FavCast,
-            status: req.body.status,
-        });
-        const s = await series.save()
-        res.status(200).json(s)
-    } catch(err) {
-        console.log(err.message);
-    }
+exports.postSeries = async (req, res) => {
+  try {
+    const series = new Series({
+      image: req.file.path,
+      name: req.body.name,
+      genre: req.body.genre,
+      FavCast: req.body.FavCast,
+      status: req.body.status,
+    });
+    const s = await series.save()
+    res.status(200).json(s)
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 exports.getAllSeries = async (req, res) => {
-    try {
-      const series = await Series.find().limit(18).exec();
-      if (!series.length) return res.json([]);
-     const result = await res.paginatedResults
-      res.status(200).json({
-        message: "Series retrieved successfully",
-        result,
-        // count: series.length,
-        // series
-      });
-  
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  try {
+    const series = await Series.find().exec();
+    if (!series.length) return res.json([]);
+    count = series.length
+    const result = await res.paginatedResults
+    res.status(200).json({
+      message: "Series retrieved successfully",
+      count,
+      result,
+      // count: series.length,
+      // series
+    });
 
-  exports.getSeries = async(req, res) => {
-
+  } catch (err) {
+    console.log(err.message);
   }
+};
 
-  exports.updateSeries = async (req, res) => {
-    try {
-      if (Object.keys(req.body).length === 0) {
-        return res.status(400).json({
-          message: "Data to update can not be empty!",
-        });
-      }
-      const id = req.params.id;
-      await Series.findByIdAndUpdate(id, req.body).then((data) => {
-        if (!data) {
-          res.json({
-            message: `Cannot update Series with id=${id}. Maybe Product was not found!`,
-          });
-        } else res.json({ message: "Series was updated successfully." });
+exports.getSeries = async (req, res) => {
+
+}
+
+exports.updateSeries = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "Data to update can not be empty!",
       });
-    } catch (err) {
-      console.log(err.message);
-      res.json("error");
     }
-  }
-
-  exports.deleteAllSeries = async (req, res) => {
-    try {
-      await Series.deleteMany({}).then((data) => {
+    const id = req.params.id;
+    await Series.findByIdAndUpdate(id, req.body).then((data) => {
+      if (!data) {
         res.json({
-          message: `${data.deletedCount} Series were deleted successfully!`,
+          message: `Cannot update Series with id=${id}. Maybe Product was not found!`,
         });
-      });
-    } catch (err) {
-      console.log(err.message);
-      res.status(400).json({
-        message:
-          err.message || "Some error occurred while removing all Series.",
-      });
-    }
+      } else res.json({ message: "Series was updated successfully." });
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.json("error");
   }
+}
 
-  exports.deleteSeries = async (req, res) => {
+exports.deleteAllSeries = async (req, res) => {
+  try {
+    await Series.deleteMany({}).then((data) => {
+      res.json({
+        message: `${data.deletedCount} Series were deleted successfully!`,
+      });
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({
+      message:
+        err.message || "Some error occurred while removing all Series.",
+    });
+  }
+}
+
+exports.deleteSeries = async (req, res) => {
 
 }
 
 
-  
