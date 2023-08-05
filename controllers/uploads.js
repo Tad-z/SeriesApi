1c38a518dcb61f7c395ea7a606b64a6a1343c348
Tad-z <david.akintade1000@gmail.com>
2023-08-05 16:13:01 +0100

    deploy

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,6 +2,8 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
+  // destination: function (req, file, cb) {
+  //   cb(null, './uploads');
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, 'uploads'));
   },
